1c9c41787d52e505c1730929877738d7e1153301
RubensSoft <joserubencastrosoriano@gmail.com>
2021-01-08 12:22:02 +0100

    Simple DataBInding with Button and ProgressBar
    - Add a button and a progress bar int the xml
    - Add a setOnClickListener to the binding button and manage the progress bar state and the text button with each click on the button

--- app/src/main/java/com/example/androidmasterclass/MainActivity.kt
+++ app/src/main/java/com/example/androidmasterclass/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidmasterclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -18,11 +19,27 @@ class MainActivity : AppCompatActivity() {
         binding.submitButton.setOnClickListener {
             displayGreeting()
         }
+
+        binding.startButton.setOnClickListener {
+            startOrStopProgressBar()
+        }
     }
 
     private fun displayGreeting() {
         binding.apply {
             greetingTextView.text = "Hello! " + nameEditText.text
+        }
+    }
+
+    private fun startOrStopProgressBar() {
+        binding.apply {
+            if (progressBar.visibility == View.GONE) {
+                progressBar.visibility = View.VISIBLE
+                startButton.text = "STOP"
+            } else {
+                progressBar.visibility = View.GONE
+                startButton.text = "START"
+            }
         }
     }
 }
