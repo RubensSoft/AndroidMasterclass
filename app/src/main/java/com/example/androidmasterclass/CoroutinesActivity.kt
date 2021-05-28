9f1414825921fd6ce199c168e358b3598b461766
RubensSoft <joserubencastrosoriano@gmail.com>
2021-05-28 13:27:22 +0200

    Run some task sequentially and concurrently Run some tasks using launch and see that tasks are executed sequentially Run some tasks using async and await (in a launch block) and see that tasks are executed concurrently (at the same time) - Launch a coroutine in a background thread belong to IO thread pool and get all concurrent executions - Launch a coroutine in Main thread that launch concurrent events using async(IO), only these events will happen in the background

--- app/src/main/java/com/example/androidmasterclass/CoroutinesActivity.kt
+++ app/src/main/java/com/example/androidmasterclass/CoroutinesActivity.kt
@@ -2,12 +2,10 @@ package com.example.androidmasterclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class CoroutinesActivity : AppCompatActivity() {
     private var count = 0
@@ -27,8 +25,36 @@ class CoroutinesActivity : AppCompatActivity() {
         }
 
         downloadDataButton.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadData()
+//            // sequentially
+//            Log.d("i", "Calculation sequentially started...")
+//            CoroutineScope(Dispatchers.IO).launch {
+//                val result1 = task1()
+//                val result2 = task2()
+//                val result3 = task3()
+//                val total = result1 + result2 + result3
+//                Log.d("i", "Total is $total")
+//            }
+
+//            // concurrently (option 1)
+//            // Launch a coroutine in a background thread belong to IO thread pool and get all concurrent executions
+//            Log.d("i", "Calculation concurrently started...")
+//            CoroutineScope(Dispatchers.IO).launch {
+//                val result1 = async { task1() }
+//                val result2 = async { task2() }
+//                val result3 = async { task3() }
+//                val total = result1.await() + result2.await() + result3.await()
+//                Log.d("i", "Total is $total")
+//            }
+
+            // concurrently (option 2)
+            // Launch a coroutine in Main thread that launch concurrent events using async(IO)
+            Log.d("i", "Calculation concurrently started...")
+            CoroutineScope(Dispatchers.Main).launch {
+                val result1 = async(Dispatchers.IO) { task1() }
+                val result2 = async(Dispatchers.IO) { task2() }
+                val result3 = async(Dispatchers.IO) { task3() }
+                val total = result1.await() + result2.await() + result3.await()
+                Log.d("i", "Total is $total")
             }
         }
     }
@@ -42,4 +68,21 @@ class CoroutinesActivity : AppCompatActivity() {
         }
     }
 
+    private suspend fun task1() : Int {
+        delay(10000)
+        Log.d("i", "task1 returned")
+        return 1
+    }
+
+    private suspend fun task2() : Int {
+        delay(8000)
+        Log.d("i", "task2 returned")
+        return 2
+    }
+
+    private suspend fun task3() : Int {
+        delay(12000)
+        Log.d("i", "task3 returned")
+        return 3
+    }
 }
