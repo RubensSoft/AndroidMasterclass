7ff903ee90de1324df56793740602adf70ef5d2d
Rubén <joseruben.castro@bestsecret.com>
2021-08-27 14:47:38 +0200

    Retrofit - add dependencies and internet permissions

---
+++ app/src/main/java/com/example/androidmasterclass/retrofit/RetrofitDemo.kt
@@ -0,0 +1,12 @@
+package com.example.androidmasterclass.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmasterclass.R
+
+class RetrofitDemo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit_demo)
+    }
+}
