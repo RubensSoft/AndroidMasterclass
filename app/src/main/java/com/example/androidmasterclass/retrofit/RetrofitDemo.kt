0a5dace05408e7186c827e7295b7a7a49c3a7214
Rubén <joseruben.castro@bestsecret.com>
2021-09-07 10:36:05 +0200

    Retrofit - request data using the service and instance, with live data and show the result on a textview

--- app/src/main/java/com/example/androidmasterclass/retrofit/RetrofitDemo.kt
+++ app/src/main/java/com/example/androidmasterclass/retrofit/RetrofitDemo.kt
@@ -1,12 +1,38 @@
 package com.example.androidmasterclass.retrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import com.example.androidmasterclass.R
+import retrofit2.Response
 
 class RetrofitDemo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit_demo)
+
+        val retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        val responseLiveData : LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, {
+            val albumList = it.body()?.listIterator()
+            albumList?.let {
+                while (albumList.hasNext()) {
+                    val albumItem = albumList.next()
+
+                    val result = "Album Title : ${albumItem.title}\n" +
+                        "Album Id : ${albumItem.id}\n" +
+                        "User Id : ${albumItem.userId}\n\n\n"
+
+                    findViewById<TextView>(R.id.textView_retrofitDemo).append(result)
+                }
+            }
+        })
     }
 }
