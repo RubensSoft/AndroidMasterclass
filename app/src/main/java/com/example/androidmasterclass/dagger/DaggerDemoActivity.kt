1207a0a32c166e9048828c85922baf3837a6907f
Rubén <joseruben.castro@bestsecret.com>
2021-12-10 14:07:21 +0100

    Dagger - use dagger generated component to create a instance of smartphone

--- app/src/main/java/com/example/androidmasterclass/dagger/DaggerDemoActivity.kt
+++ app/src/main/java/com/example/androidmasterclass/dagger/DaggerDemoActivity.kt
@@ -6,8 +6,12 @@ import com.example.androidmasterclass.R
 
 class DaggerDemoActivity : AppCompatActivity() {
     private lateinit var smartphone: SmartPhone
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dagger_demo)
+
+        smartphone = DaggerSmartPhoneComponent.create().provideSmartPhone()
+        smartphone.makeACall()
     }
 }
