3cdec4b52da2c661419e40463e6edf48547d4806
Rubén <joseruben.castro@bestsecret.com>
2021-12-10 12:35:33 +0100

    Dagger - add dependencies to use dagger and create a empty activity to play with dagger

---
+++ app/src/main/java/com/example/androidmasterclass/dagger/DaggerDemoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidmasterclass.dagger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmasterclass.R
+
+class DaggerDemoActivity : AppCompatActivity() {
+    private lateinit var smartphone: SmartPhone
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger_demo)
+    }
+}
---
+++ app/src/main/java/com/example/androidmasterclass/dagger/SmartPhone.kt
@@ -0,0 +1,4 @@
+package com.example.androidmasterclass.dagger
+
+class SmartPhone {
+}
