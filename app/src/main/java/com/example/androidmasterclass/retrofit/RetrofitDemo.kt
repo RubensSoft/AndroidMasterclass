be89791653e3d907d00d37b29d6d9a8d4dab7794
Rubén <joseruben.castro@bestsecret.com>
2021-12-10 10:54:28 +0100

    Retrofit - refactor

--- app/src/main/java/com/example/androidmasterclass/retrofit/RetrofitDemo.kt
+++ app/src/main/java/com/example/androidmasterclass/retrofit/RetrofitDemo.kt
@@ -10,35 +10,42 @@ import com.example.androidmasterclass.R
 import retrofit2.Response
 
 class RetrofitDemo : AppCompatActivity() {
+
+    private lateinit var retrofitService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit_demo)
+        retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        getRequestWithQueryParameteres()
+        getRequestWithPathParameters()
+    }
 
-        val retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+    private fun getRequestWithQueryParameteres() {
         val responseLiveData : LiveData<Response<Albums>> = liveData {
 //            val response = retrofitService.getAlbums()
             val response = retrofitService.getSortedAlbums(userId = 3)
             emit(response)
         }
-        val pathResponse : LiveData<Response<AlbumItem>> = liveData {
-            val response = retrofitService.getAlbum(3)
-            emit(response)
-        }
-
         responseLiveData.observe(this, {
             val albumList = it.body()?.listIterator()
             albumList?.let {
                 while (albumList.hasNext()) {
                     val albumItem = albumList.next()
-
                     val result = "Album Title : ${albumItem.title}\n" +
                         "Album Id : ${albumItem.id}\n" +
                         "User Id : ${albumItem.userId}\n\n\n"
-
-//                    findViewById<TextView>(R.id.textView_retrofitDemo).append(result)
+                    findViewById<TextView>(R.id.textView_retrofitDemo).append(result)
                 }
             }
         })
+    }
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse : LiveData<Response<AlbumItem>> = liveData {
+            val response = retrofitService.getAlbum(3)
+            emit(response)
+        }
         pathResponse.observe(this, {
             val title = it.body()?.title
             findViewById<TextView>(R.id.textView_retrofitDemo).append(title)
