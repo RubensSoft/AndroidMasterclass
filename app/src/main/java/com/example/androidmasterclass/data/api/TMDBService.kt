9012da2648fe8a805548411d99ea18e2413a4ae1
Rubén <joseruben.castro@bestsecret.com>
2022-01-28 11:21:27 +0100

    Add data interface with get methods to retrieve movies, tv shows and persons list.

---
+++ app/src/main/java/com/example/androidmasterclass/data/api/TMDBService.kt
@@ -0,0 +1,20 @@
+package com.example.androidmasterclass.data.api
+
+import com.example.androidmasterclass.data.ArtistList
+import com.example.androidmasterclass.data.MovieList
+import com.example.androidmasterclass.data.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTVShows(@Query("api_key") apiKey: String): Response<TvShowList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtists(@Query("api_key") apiKey: String): Response<ArtistList>
+}
